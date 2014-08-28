f2179f60f7d1c4dba5e7fa121ed7e49e2b232cd5
Paul Thompson <paulkth73@gmail.com>
2014-08-28 14:39:13 -0500

    Added Empty Project

---
+++ EmptyServiceStack/EmptyServiceStack/EmptyServiceStack.ServiceInterface/MyServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using EmptyServiceStack.ServiceModel;
+
+namespace EmptyServiceStack.ServiceInterface
+{
+	public class MyServices : Service
+	{
+		public object Any(Hello request)
+		{
+			return new HelloResponse
+			{
+				Result = "Hello, {0}!".Fmt(request.Name)
+			};
+		}
+	}
+}
---
+++ EmptyServiceStack/EmptyServiceStack/EmptyServiceStack.Tests/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+using EmptyServiceStack.ServiceInterface;
+using EmptyServiceStack.ServiceModel;
+using ServiceStack.Testing;
+using ServiceStack;
+
+namespace EmptyServiceStack.Tests
+{
+	[TestFixture]
+	public class UnitTests
+	{
+		private readonly ServiceStackHost appHost;
+
+		public UnitTests()
+		{
+			appHost = new BasicAppHost(typeof(MyServices).Assembly)
+			{
+				ConfigureContainer = container =>
+				{
+					//Add your IoC dependencies here
+				}
+			}
+			.Init();
+		}
+
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown()
+		{
+			appHost.Dispose();
+		}
+
+		[Test]
+		public void TestMethod1()
+		{
+			var service = appHost.Container.Resolve<MyServices>();
+
+			var response = (HelloResponse)service.Any(new Hello
+			{
+				Name = "World"
+			});
+
+			Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+		}
+	}
+}
