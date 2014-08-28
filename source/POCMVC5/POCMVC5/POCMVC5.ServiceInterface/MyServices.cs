eb9285fb88d41a53f295b057fcd894b1e7c323dd
Paul Thompson <paulkth73@gmail.com>
2014-08-28 12:59:38 -0500

    mvc5 template
    added the servicestack mvc5 template project

---
+++ source/POCMVC5/POCMVC5/POCMVC5.ServiceInterface/MyServices.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ServiceStack;
+using POCMVC5.ServiceModel;
+
+namespace POCMVC5.ServiceInterface
+{
+	public class MyServices : Service
+	{
+		public object Any(Hello request)
+		{
+			return new HelloResponse
+			{
+				Result = "Hello, {0}!".Fmt(request.Name)
+			};
+		}
+	}
+}
---
+++ source/POCMVC5/POCMVC5/POCMVC5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="POCMVC5.MvcApplication" Language="C#" %>
